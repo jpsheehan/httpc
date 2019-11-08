de586895b94e0c933f6e634357256ec254896e76
JP Sheehan <jesse@sheehan.nz>
2019-11-08 22:32:32 +1300

    Removed all printfs and redirected them to the logger.

--- dispatcher.c
+++ dispatcher.c
@@ -9,6 +9,8 @@
 #include "network.h"
 #include "logger.h"
 
+#define DISPATCHER_BUFFER_SIZE 64
+
 /**
  * Called automatically when the thread is cancelled/exited.
  */
@@ -31,7 +33,7 @@ void dispatcher_thread_cleanup(void *t_args)
 
 void *dispatcher_thread_worker(void *t_args)
 {
-    char buffer[64] = {0};
+    char buffer[DISPATCHER_BUFFER_SIZE] = {0};
     dispatcher_thread_args_t *args = (dispatcher_thread_args_t *)t_args;
 
     // add the cleanup handler
@@ -40,12 +42,12 @@ void *dispatcher_thread_worker(void *t_args)
     // attempt to create the socket, exiting upon failure
     if ((args->sock_fd = network_get_socket(args->port)) < 0)
     {
-        snprintf(buffer, 64, "Dispatcher couldn't open port %d", args->port);
+        snprintf(buffer, DISPATCHER_BUFFER_SIZE, "Dispatcher couldn't open port %d", args->port);
         logger_err(args->logger_queue, LOG_SRC_DISPATCH, args->thread_id, buffer);
         pthread_exit(NULL);
     }
 
-    snprintf(buffer, 64, "Dispatcher thread listening on port %d", args->port);
+    snprintf(buffer, DISPATCHER_BUFFER_SIZE, "Dispatcher thread listening on port %d", args->port);
 
     // print a nice message
     logger_info(args->logger_queue, LOG_SRC_DISPATCH, args->thread_id, buffer);
@@ -58,7 +60,8 @@ void *dispatcher_thread_worker(void *t_args)
         // attempt to accept the connection
         if ((args->__conn->sock_fd = accept(args->sock_fd, (struct sockaddr *)&args->__conn->addr, (socklen_t *)&args->__conn->addr_len)) < 0)
         {
-            fprintf(stderr, "Failed to accept connection on thread %d (port %d)\n", args->thread_id, args->port);
+            snprintf(buffer, DISPATCHER_BUFFER_SIZE, "Failed to accept connection on port %d", args->port);
+            logger_warn(args->logger_queue, LOG_SRC_DISPATCH, args->thread_id, buffer);
             free(args->__conn);
             args->__conn = NULL;
             continue;
